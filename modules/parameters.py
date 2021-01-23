e5439c1eaa80dca7f5100d9fe28193303809d7ae
billofbong <will@wright.lv>
2021-01-22 18:03:17 -0800

    Fixed "=" in keys.txt breaking parsing

--- modules/parameters.py
+++ modules/parameters.py
@@ -3,6 +3,6 @@ params = {}
 with open("keys.txt", "r") as file:
     lines = file.read().splitlines()
     for line in lines:
-        x = line.split("=")
-        if len(x) == 2:
+        x = line.split("=", 1)
+        if len(x) >= 2:
             params[x[0]] = x[1].strip()
